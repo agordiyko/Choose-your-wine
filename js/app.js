83a6cfa3ff35a51d52adba7dab4f2855863c979d
agordiyko <gordiyko.andrey@gmail.com>
2021-08-15 16:59:36 +0300

    much optimized code

--- js/app.js
+++ js/app.js
@@ -2,35 +2,39 @@ let select = document.getElementById('select');
 let options = document.querySelectorAll('.content__option');
 let result = document.querySelector('.result');
 let hold = document.querySelector('.hold');
-let resultList = document.querySelector(".result__list");
+let resultList = document.querySelector('.result__list');
 
-function removeResult() {
-	result.remove();
-};
-function createResult() {
-	result = document.createElement('div');
-	result.classList.add('result');
-	hold.prepend(result);
-};
-function removeResultList() {
-	resultList.remove();
-};
-function createResultList() {
-	resultList = document.createElement('ul');
-	resultList.classList.add('result__list');
-	result.prepend(resultList);
-};
-
-function createTitle() {
-	let resultTitle = document.createElement('h3');
-	resultTitle.classList.add('result__title');
-	resultTitle.innerHTML = 'Подходящее для вашего продукта вино:';
-	result.prepend(resultTitle);
-};
 function removeTitle() {
 	resultTitle.remove();
 };
 
+function showData() {
+	result.remove();
+
+	result = document.createElement('div');
+	result.classList.add('result');
+	hold.prepend(result);
+
+	resultList.remove();
+	
+	resultList = document.createElement('ul');
+	resultList.classList.add('result__list');
+	result.prepend(resultList);
+
+	let resultTitle = document.createElement('h3');
+	resultTitle.classList.add('result__title');
+	resultTitle.innerHTML = 'Подходящее для вашего продукта вино:';
+	result.prepend(resultTitle);
+}
+
+function createItem(item) {
+	for (let i = 1; i < 2; i++) {
+		let resultItem = document.createElement('li');
+		resultItem.classList.add('result__item');
+		resultItem.innerHTML = `${item}`;
+		resultList.append(resultItem);
+	}
+}
 
 select.addEventListener('change', () => {
 
@@ -38,357 +42,95 @@ select.addEventListener('change', () => {
 
 
 	if (data === "red-meat") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id1.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "salted-smoked-meat") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id1.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id6.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "white-meat") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id6.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "crustacean-mollusc") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id7.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "fish") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id6.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id7.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "hard-cheese") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id1.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id9.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "soft-cheese") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id6.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id7.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id9.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "vegetables-greens") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id4.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id7.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "exotic-spices") {
-		removeResult()
-		createResult();
-		removeResultList();
-		createResultList();
-		createTitle();
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id2.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id3.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id8.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id9.name;
-			resultList.append(resultItem);
-		}
-	} else if (data === "dessert") {
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id5.name;
-			resultList.append(resultItem);
-		}
-		for (let i = 1; i < 2; i++) {
-			let resultItem = document.createElement('li');
-			resultItem.classList.add('result__item');
-			resultItem.innerHTML = wines.id9.name;
-			resultList.append(resultItem);
-		}
+		showData(); 
+		createItem(wines.id1.name);
+		console.log(fullBodiedRed);
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		return;
+	} 
+	if (data === "salted-smoked-meat") {
+		showData();
+		createItem(wines.id1.name);
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		createItem(wines.id4.name);
+		createItem(wines.id5.name);
+		createItem(wines.id6.name);
+		createItem(wines.id8.name);
+		return;
+	} 
+	if (data === "white-meat") {
+		showData();
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		createItem(wines.id4.name);
+		createItem(wines.id5.name);
+		createItem(wines.id6.name);
+		createItem(wines.id8.name);
+		return;
+	} 
+	if (data === "crustacean-mollusc") {
+		showData();
+		createItem(wines.id4.name);
+		createItem(wines.id5.name);
+		createItem(wines.id7.name);
+		createItem(wines.id8.name);
+		return;
+	}  
+	if (data === "fish") {
+		showData();
+		createItem(wines.id4.name);
+		createItem(wines.id6.name);
+		createItem(wines.id7.name);
+		createItem(wines.id8.name);
+		return;
+	} 
+	if (data === "hard-cheese") {
+		showData();
+		createItem(wines.id1.name);
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		createItem(wines.id4.name);
+		createItem(wines.id5.name);
+		createItem(wines.id8.name);
+		createItem(wines.id9.name);
+		return;
+	} 
+	if (data === "soft-cheese") {
+		showData();
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		createItem(wines.id4.name);
+		createItem(wines.id5.name);
+		createItem(wines.id6.name);
+		createItem(wines.id7.name);
+		createItem(wines.id8.name);
+		createItem(wines.id9.name);
+		return;
+	}  
+	if (data === "vegetables-greens") {
+		showData();
+		createItem(wines.id4.name);
+		createItem(wines.id7.name);
+		createItem(wines.id8.name);
+		return;
+	} 
+	if (data === "exotic-spices") {
+		showData();
+		createItem(wines.id2.name);
+		createItem(wines.id3.name);
+		createItem(wines.id5.name);
+		createItem(wines.id8.name);
+		createItem(wines.id9.name);
+		return;
+	} 
+	if (data === "dessert") {
+		showData();
+		createItem(wines.id5.name);
+		createItem(wines.id9.name);
+		return;
 	}
-
-
 }
 )
 
